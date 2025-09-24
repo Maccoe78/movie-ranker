cdad363e933dce963391b85bcd5e738df093c4e7
Maccoe78 <macfontys@gmail.com>
2025-09-24 14:50:23 +0200

    test voor pipelines

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload je mdr.
+          Edit <code>src/App.js</code> and save to reload. hallo
         </p>
         <a
           className="App-link"
